9ee488db793aa702222fe562ea1ab47d2306bba7
John Bidlack <82181084+JBidlack@users.noreply.github.com>
2025-09-10 10:27:21 -0400

    Add files via upload

--- main/modes/system/mainMenu/mainMenu.c
+++ main/modes/system/mainMenu/mainMenu.c
@@ -177,8 +177,8 @@ trophyDataList_t menuTrophyData = {
 };
 
 // It's good practice to declare immutable strings as const so they get placed in ROM, not RAM
-const char mainMenuName[]                       = "Main Menu";
-const char mainMenuTitle[]                      = "Swadge";
+const char mainMenuName[]                       = "Main Menu is Full";
+const char mainMenuTitle[]                      = "Badge";
 static const char mainMenuShowSecretsMenuName[] = "Secrets In Menu: ";
 static const char factoryResetName[]            = "Factory Reset";
 static const char confirmResetName[]            = "! Confirm Reset !";
